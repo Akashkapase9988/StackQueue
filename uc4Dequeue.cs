7e41a81fbf6e193fe758d7639c7899335df2e96a
Akashkapase9988 <akashkapase9988@gmail.com>
2021-10-01 13:00:19 +0530

    writing program for queue for dqQueue the queue

---
+++ uc4Dequeue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day14stackqueue
+{
+    class uc4Dequeue
+    {
+
+
+
+        public static void Dequeue()
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+
+            // A queue can be enumerated without disturbing its contents.
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+            Console.WriteLine("Peek at next item to dequeue: {0}",
+                numbers.Peek());
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+            
+        }
+    }
+}
