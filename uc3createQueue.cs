c7592e8a24dd18dd328116c2aa2219b3dd60a8e8
Akashkapase9988 <akashkapase9988@gmail.com>
2021-10-01 12:59:45 +0530

    writing program for queue for creating queue

---
+++ uc3createQueue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day14stackqueue
+{
+
+
+    class Queue
+    {
+        public static void createqueue()
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+
+            // A queue can be enumerated without disturbing its contents.
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
+
+
