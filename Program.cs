8add1f1ed8bb7dc727ad9e7d90ab768f8a85f158
Akashkapase9988 <akashkapase9988@gmail.com>
2021-10-01 12:45:28 +0530

    writing program for stack for calling function or method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day14stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
